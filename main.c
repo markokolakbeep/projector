4ff17fff699a56b0d3de8e85ff4043bbc68b3b66
Karlo Pervan <karlopervan4@gmail.com>
2025-12-22 11:58:37 +0100

    Added flags, fixed main

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <SDL2/SDL.h>
 #include <stdlib.h>
-int main(int argc, char argv[])
+int main(int argc, char* argv[])
 {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window window = SDL_CreateWindow("3d projector",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800, 600,SDL_WINDOW_SHOWN);
-    SDL_delay(4000);
-    SDl_DestroyWindow(window);
-    SDl_Quit();
+    SDL_Window* window = SDL_CreateWindow("3d projector",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800, 600,SDL_WINDOW_SHOWN);
+    SDL_Delay(4000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
