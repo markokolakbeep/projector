10dff69645e950692b4ee27d8371b9472985c86d
kpervan <karlopervan4@gmail.com>
2026-02-28 14:33:06 +0100

    Add map, remove sdl

--- main.c
+++ main.c
@@ -1,74 +1,29 @@
-#include <SDL2/SDL.h>
 #include <stdlib.h>
 
 #define WIDTH 800
 #define HEIGHT 600
 
+#define mapWidth 10
+#define mapHight 10
+#define mapSize 64
 int main(int argc, char* argv[])
 {
-	  int running = 1;
-    
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("3d projector",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT,SDL_WINDOW_SHOWN);
+	
+	int worldMap[mapWidth][mapHight] = {
+		{1,1,1,1,1,1,1,1,1,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,1,1,0,0,0,1},
+		{1,0,0,0,1,1,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,1,1,1,1,1,1,1,1,1},
+	}
 
-    SDL_Rect rect = {WIDTH/2-50,HEIGHT/2-50, 100, 100};
-    SDL_Surface *screenSurface = SDL_GetWindowSurface(window);
-    SDL_UpdateWindowSurface(window);  
-    SDL_Event event; 
-    typedef struct {
-        float x;
-        float y;
-        float z;
-    
+	:	
   
-    } Vec3;
-    typedef struct{
-        Vec3 position;
-        float yaw;
-        float pitch;
-        float speed;
-        float sens;
 
-
-
-    } Camera;
-    while(running) {
-      while (SDL_PollEvent(&event)) {
-        switch(event.type) {
-          case SDL_QUIT:
-            running = 0;
-            break;
-
-           case SDL_KEYDOWN:
-              
-              SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface-> format, 0x00, 0x00, 0x00));
-              switch(event.key.keysym.sym) {
-                
-                case SDLK_w:
-                    rect.y -= 10;
-                    break;
-             
-                case SDLK_s:
-                    rect.y += 10;
-                    break;
-
-                
-                case SDLK_d:
-                    rect.x += 10;
-                    break;
-
-                case SDLK_a:
-                    rect.x -= 10;
-                    break;
-              }
-           break;
-        }
-           
-        SDL_FillRect(screenSurface, &rect, SDL_MapRGB(screenSurface -> format, 0xff, 0xff, 0xff));     
-        SDL_UpdateWindowSurface(window);
-      }
-    }
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+	int running = 1;
     return 0;
 }
